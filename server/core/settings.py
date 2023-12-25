648ea6bbea354c43ed0d970cbafc0b03316402f1
luchompi <lpichon63@gmail.com>
2023-12-25 13:26:40 -0500

    feat(Ver. 0.1.0.0): Server - 🛠️Configuración de servidor:
    Se han añadido configuraciones para desarrollo

--- server/core/settings.py
+++ server/core/settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         #test config
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': os.getenv('DB_NAME'),
+        #'USER': os.getenv('DB_USER'),
+        #'PASSWORD': os.getenv('DB_PASSWORD'),
+        #'HOST': os.getenv('DB_HOST'),
+        #'PORT': os.getenv('DB_PORT'),
 
-        #dev config
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+        # dev config
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
