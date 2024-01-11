d10127b952d20d0975a9fe2227f53b09c54490f9
luchompi <lpichon63@gmail.com>
2024-01-11 14:47:30 -0500

    feat(Ver. 0.1.0.2):  ✨️ UI:
    rutas:  se han creado rutas para el uso del proyecto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar.jsx";
+import sesionStore from "./stores/sesion.store.js";
+import Loading from "./components/Loading.jsx";
+
 const App = () => {
-  return <div>App</div>;
+  const [dateTime, setDateTime] = useState("");
+  const { loading } = sesionStore((state) => state);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime(new Date().toLocaleString());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <>
+      <Navbar dateTime={dateTime} />
+      <div className="row justify-content-center mt-2">
+        {loading ? <Loading /> : <Outlet />}
+      </div>
+    </>
+  );
 };
 
 export default App;
--- client/src/routes/Root.js
+++ client/src/routes/Root.js
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import App from '../App'
+import authRoutes from "./auth.login.js";
+import principalRoutes from './principal.router.js';
 
 const Root = createBrowserRouter([
     {
         path: '/',
-        component: App,
+        Component: App,
+        children: [
+            ...Array.from(authRoutes),
+            ...Array.from(principalRoutes)
+        ]
     }
 ])
 
