b0e5b7bce36a0188c056d7a5ff3a20ba7fc8a33e
luchompi <lpichon63@gmail.com>
2024-01-13 10:46:49 -0500

    fix(Ver. 0.1.0.2) ✨UI - Redirección de sesión:
    Se añadido a la aplicación de inventario, hook que permite redireccionar a login si la sesión no se encuentra activa.

--- client/src/pages/inventario/Update.jsx
+++ client/src/pages/inventario/Update.jsx
@@ -1,42 +1,45 @@
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import CardLayout from "../../layouts/CardLayout";
 import EditDocument from "../../assets/json/EditDocument.json";
 import inventarioStore from "../../stores/inventario.store";
 import Form from "./Form";
-import { useEffect } from "react";
+import {useEffect} from "react";
 import EventEmitter from "../../services/EventEmitter";
 import InventarioHook from "../../hooks/InventarioHook";
+import {RedirectIfAuthRequired} from "../../hooks/SesionRedirector.jsx";
+
 const UpdateComponent = () => {
-  const { id } = useParams();
-  const { inventario } = inventarioStore((state) => state);
-  const producto = inventario.find((item) => item.id === parseInt(id));
-  const { actualizarElemento } = InventarioHook();
-  useEffect(() => {
-    EventEmitter.addListener("update", (data) => {
-      actualizarElemento(id, data);
-    });
-    return () => {
-      EventEmitter.removeAllListeners("create");
-    };
-  }, []);
-  return (
-    <>
-      <Form producto={producto} />
-    </>
-  );
+    const {id} = useParams();
+    const {inventario} = inventarioStore((state) => state);
+    const producto = inventario.find((item) => item.id === parseInt(id));
+    const {actualizarElemento} = InventarioHook();
+    useEffect(() => {
+        EventEmitter.addListener("update", (data) => {
+            actualizarElemento(id, data);
+        });
+        return () => {
+            EventEmitter.removeAllListeners("create");
+        };
+    }, []);
+    return (
+        <>
+            <Form producto={producto}/>
+        </>
+    );
 };
 
 const Update = () => {
-  return (
-    <div>
-      <CardLayout
-        title="Editar producto"
-        header="Actualizar detalles de producto"
-        component={<UpdateComponent />}
-        icon={EditDocument}
-      />
-    </div>
-  );
+    RedirectIfAuthRequired()
+    return (
+        <div>
+            <CardLayout
+                title="Editar producto"
+                header="Actualizar detalles de producto"
+                component={<UpdateComponent/>}
+                icon={EditDocument}
+            />
+        </div>
+    );
 };
 
 export default Update;
