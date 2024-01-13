1d76818f4d3863e7415f6b0656f1fb82b20f22fb
luchompi <lpichon63@gmail.com>
2024-01-13 17:57:54 -0500

    feat(Ver. 0.1.0.2) ✨UI - Dashboard.jsx:
    Se ha habilitado comprobación de inventario, ahora se muestran mensajes de los elementos que cumplan criterios para mostrarse con pocas o ninguna unidad

--- client/src/pages/principal/Dashboard.jsx
+++ client/src/pages/principal/Dashboard.jsx
@@ -4,17 +4,21 @@ import ICON from "../../assets/json/Icon.json";
 import BoxWarning from "../../components/BoxWarning";
 import BoxDanger from "../../components/BoxDanger";
 import sesionStore from "../../stores/sesion.store";
+import inventarioStore from "../../stores/inventario.store.js";
 
 const DashboardComponent = () => {
+    const {inventario} = inventarioStore((state) => state);
+    const lowUnits = inventario.filter((item) => item.cantidad <= 20);
+    const outUnits = inventario.filter((item) => item.cantidad === 0);
     return (
         <>
             <h6>Hasta ahora, de los elementos en sistema</h6>
             <div className="row justify-content-md-center">
                 <div className="col col-lg-5">
-                    <BoxWarning/>
+                    <BoxWarning productos={lowUnits}/>
                 </div>
                 <div className="col col-lg-5">
-                    <BoxDanger/>
+                    <BoxDanger productos={outUnits}/>
                 </div>
             </div>
         </>
