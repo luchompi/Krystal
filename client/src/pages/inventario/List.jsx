4f6095f28061ef8267af52e1959b2c00d8adf07b
luchompi <lpichon63@gmail.com>
2024-01-14 19:00:08 -0500

    feat(Ver. 0.1.0.3): :sparkles: UI - Exportador de excel
    Se ha creado botón que ejecuta tarea que exporta excel

--- client/src/pages/inventario/List.jsx
+++ client/src/pages/inventario/List.jsx
@@ -1,109 +1,155 @@
-import React from "react";
+import * as XLSX from "xlsx";
 import inventarioStore from "../../stores/inventario.store";
 import CardLayout from "../../layouts/CardLayout";
 import Chart from "../../assets/json/Chart.json";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import InventarioHook from "../../hooks/InventarioHook";
-import {RedirectIfAuthRequired} from "../../hooks/SesionRedirector.jsx";
+import { RedirectIfAuthRequired } from "../../hooks/SesionRedirector.jsx";
 
 const ListComponent = () => {
-    const {inventario} = inventarioStore((state) => state);
-    const {eliminarElemento} = InventarioHook();
-    return (
-        <>
-            <div className="card">
-                <div className="card-body">
-                    <div className="card-text">
-                        <table className="table table-striped table-hover">
-                            <thead>
-                            <tr>
-                                <th scope="col">Cód</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Cantidad</th>
-                                <th scope="col">Costo</th>
-                                <th scope="col">Precio unitario</th>
-                                <th scope="col">Precio de Venta</th>
-                                <th scope="col">Ganancia</th>
-                                <th scope="col">Opciones</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {inventario.map((item) => {
-                                return (
-                                    <tr key={item.id}>
-                                        <th scope="row">{item.id}</th>
-                                        <td>{item.nombre}</td>
-                                        <td>{item.cantidad}</td>
-                                        <td>$ {item.precio}</td>
-                                        <td>$ {item.precio_unitario}</td>
-                                        <td>$ {item.precio_venta}</td>
-                                        <td>{item.ganancia} %</td>
-                                        <td>
-                                            <div
-                                                className="btn-group"
-                                                role="group"
-                                                aria-label="Basic example"
-                                            >
-                                                <Link
-                                                    to={`/inventario/editar/${item.id}`}
-                                                    type="button"
-                                                    className="btn btn-warning"
-                                                >
-                                                    <i className="ri-pencil-line"></i>
-                                                </Link>
-                                                <button
-                                                    type="button"
-                                                    className="btn btn-danger"
-                                                    onClick={() => {
-                                                        eliminarElemento(item.id);
-                                                    }}
-                                                >
-                                                    <i className="ri-delete-bin-line"></i>
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  const { inventario } = inventarioStore((state) => state);
+  const { eliminarElemento } = InventarioHook();
+  return (
+    <>
+      <div className="card">
+        <div className="card-body">
+          <div className="card-text">
+            <table className="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Cód</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Cantidad</th>
+                  <th scope="col">Costo</th>
+                  <th scope="col">Precio unitario</th>
+                  <th scope="col">Precio de Venta</th>
+                  <th scope="col">Ganancia</th>
+                  <th scope="col">Opciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {inventario.map((item) => {
+                  return (
+                    <tr key={item.id}>
+                      <th scope="row">{item.id}</th>
+                      <td>{item.nombre}</td>
+                      <td>{item.cantidad}</td>
+                      <td>$ {item.precio}</td>
+                      <td>$ {item.precio_unitario}</td>
+                      <td>$ {item.precio_venta}</td>
+                      <td>{item.ganancia} %</td>
+                      <td>
+                        <div
+                          className="btn-group"
+                          role="group"
+                          aria-label="Basic example"
+                        >
+                          <Link
+                            to={`/inventario/editar/${item.id}`}
+                            type="button"
+                            className="btn btn-warning"
+                          >
+                            <i className="ri-pencil-line"></i>
+                          </Link>
+                          <button
+                            type="button"
+                            className="btn btn-danger"
+                            onClick={() => {
+                              eliminarElemento(item.id);
+                            }}
+                          >
+                            <i className="ri-delete-bin-line"></i>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 const NavButtons = () => {
-    RedirectIfAuthRequired()
-    return (
-        <>
-            <div className="card-tools">
-                <Link
-                    to={`/inventario/nuevo`}
-                    type="button"
-                    className="btn btn-outline-primary"
-                >
-                    Añadir
-                    <i className="ri-file-add-line"></i>
-                </Link>
-            </div>
-        </>
-    );
+  RedirectIfAuthRequired();
+  const { inventario } = inventarioStore((state) => state);
+  let wb = XLSX.utils.book_new();
+  wb.Props = {
+    Title: "Inventario",
+    Subject: "Inventario",
+    Author: "Sistema de inventario",
+    CreatedDate: new Date().toLocaleString(),
+  };
+  wb.SheetNames.push("Inventario", "Inventario general");
+  let ws_data = [];
+  let ws_general = [];
+  ws_data.push(["Código", "Nombre", "Cantidad", "Stock actual"]);
+  ws_general.push([
+    "Código",
+    "Nombre",
+    "Cantidad",
+    "Precio",
+    "Precio unitario",
+    "Precio de venta",
+    "Ganancia",
+  ]);
+  inventario.forEach((item) => {
+    ws_data.push([item.id, item.nombre, item.cantidad]);
+    ws_general.push([
+      item.id,
+      item.nombre,
+      item.cantidad,
+      item.precio,
+      item.precio_unitario,
+      item.precio_venta,
+      item.ganancia,
+    ]);
+  });
+  let ws = XLSX.utils.aoa_to_sheet(ws_data);
+  wb.Sheets["Inventario"] = ws;
+  wb.Sheets["Inventario general"] = XLSX.utils.aoa_to_sheet(ws_general);
+  return (
+    <>
+      <div className="card-tools">
+        <Link
+          to={`/inventario/nuevo`}
+          type="button"
+          className="btn btn-outline-primary"
+        >
+          Añadir
+          <i className="ri-file-add-line"></i>
+        </Link>
+        <button
+          onClick={() => {
+            XLSX.writeFile(wb, "Inventario.xlsx");
+          }}
+          type="button"
+          className="btn btn-outline-success"
+        >
+          Exportar
+          <i className="ri-file-download-line"></i>
+        </button>
+      </div>
+    </>
+  );
 };
 
 const List = () => {
-    return (
-        <div className="col col-lg-10">
-            <CardLayout
-                icon={Chart}
-                title="Inventario General de elementos"
-                header={"Listado de elementos"}
-                cardTools={<NavButtons/>}
-                component={<ListComponent/>}
-            />
-        </div>
-    );
+  return (
+    <div className="col col-lg-10">
+      <CardLayout
+        icon={Chart}
+        title="Inventario General de elementos"
+        header={"Listado de elementos"}
+        cardTools={<NavButtons />}
+        component={<ListComponent />}
+      />
+    </div>
+  );
 };
 
 export default List;
