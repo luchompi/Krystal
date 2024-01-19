941af6148639cf065b6ff2b3c8b9b36b7418a972
luchompi <lpichon63@gmail.com>
2024-01-18 19:03:18 -0500

    fix(Ver. 0.1.5): :bug: Reporte de cantidades - Dashboard
    Se ha corregido error que contaba elementos sin cantidades en pocas unidades tambien

--- client/src/pages/principal/Dashboard.jsx
+++ client/src/pages/principal/Dashboard.jsx
@@ -1,52 +1,52 @@
-import React from "react";
-import CardLayout from "../../layouts/CardLayout";
-import ICON from "../../assets/json/Icon.json";
-import BoxWarning from "../../components/BoxWarning";
-import BoxDanger from "../../components/BoxDanger";
-import sesionStore from "../../stores/sesion.store";
-import inventarioStore from "../../stores/inventario.store.js";
+import CardLayout from '../../layouts/CardLayout';
+import ICON from '../../assets/json/Icon.json';
+import BoxWarning from '../../components/BoxWarning';
+import BoxDanger from '../../components/BoxDanger';
+import sesionStore from '../../stores/sesion.store';
+import inventarioStore from '../../stores/inventario.store.js';
 
 const DashboardComponent = () => {
-    const {inventario} = inventarioStore((state) => state);
-    const lowUnits = inventario.filter((item) => item.cantidad <= 20);
-    const outUnits = inventario.filter((item) => item.cantidad === 0);
-    return (
-        <>
-            <h6>Hasta ahora, de los elementos en sistema</h6>
-            <div className="row justify-content-md-center">
-                <div className="col col-lg-5">
-                    <BoxWarning productos={lowUnits}/>
-                </div>
-                <div className="col col-lg-5">
-                    <BoxDanger productos={outUnits}/>
-                </div>
-            </div>
-        </>
-    );
+  const { inventario } = inventarioStore((state) => state);
+  const lowUnits = inventario.filter(
+    (item) => item.cantidad <= 20 && item.cantidad >= 1
+  );
+  const outUnits = inventario.filter((item) => item.cantidad === 0);
+  return (
+    <>
+      <h6>Hasta ahora, de los elementos en sistema</h6>
+      <div className="row justify-content-md-center">
+        <div className="col col-lg-5">
+          <BoxWarning productos={lowUnits} />
+        </div>
+        <div className="col col-lg-5">
+          <BoxDanger productos={outUnits} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 const Dashboard = () => {
-    const {isLogged, userData} = sesionStore((state) => state);
+  const { isLogged } = sesionStore((state) => state);
 
-
-    return (
-        <div>
-            {isLogged ? (
-                <CardLayout
-                    icon={ICON}
-                    header="Panel de administración"
-                    title="Panel de información general"
-                    component={<DashboardComponent/>}
-                />
-            ) : (
-                <CardLayout
-                    icon={ICON}
-                    header="Krystal App"
-                    title="¡Bienvenido a krystal!"
-                />
-            )}
-        </div>
-    );
+  return (
+    <div>
+      {isLogged ? (
+        <CardLayout
+          icon={ICON}
+          header="Panel de administración"
+          title="Panel de información general"
+          component={<DashboardComponent />}
+        />
+      ) : (
+        <CardLayout
+          icon={ICON}
+          header="Krystal App"
+          title="¡Bienvenido a krystal!"
+        />
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
