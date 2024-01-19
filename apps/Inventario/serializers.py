e7aaef6937b680d7d2f9668ce719cdb795e58b47
luchompi <lpichon63@gmail.com>
2024-01-18 22:45:53 -0500

    fix(🐛 Django server): Configuración base
    Se ha modificado el servidor de django, buscando solucionar error de despliegue en railway

--- apps/Inventario/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Inventario
-
-
-class InventarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Inventario
-        fields = '__all__'
