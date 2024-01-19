40dee8106bd87ebd71266bdeaa72ef25d93ccd5a
luchompi <lpichon63@gmail.com>
2024-01-19 10:15:35 -0500

    fix (Ver. 0.2.0.2): 🐛UI - API:
    Se ha corregido api

--- client/src/apis/base.api.js
+++ client/src/apis/base.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseApi = axios.create({
-    baseURL: 'https://krystal-production.up.railway.app//api/v1/',
+    baseURL: 'https://krystal-production.up.railway.app/api/v1/',
 })
 
 export default baseApi
