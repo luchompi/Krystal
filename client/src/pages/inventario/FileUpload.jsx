84e6eb6616e6d3ec33800f913c6a2dbc4eaec34e
luchompi <lpichon63@gmail.com>
2024-01-18 19:06:54 -0500

    feat(Ver. 0.1.5): :ambulance: FileUpload - Aplicado custom hook
    Se ha aplicado hook que redirecciona si la sesión no fue iniciada

--- client/src/pages/inventario/FileUpload.jsx
+++ client/src/pages/inventario/FileUpload.jsx
@@ -1,26 +1,27 @@
-import Book from "../../assets/json/Book.json";
-import CardLayout from "../../layouts/CardLayout";
-import FileComponent from "../../components/FileComponent";
-import { useEffect, useState } from "react";
-import EventEmitter from "../../services/EventEmitter";
-import ElementTable from "../../components/ElementTable";
-
+import Book from '../../assets/json/Book.json';
+import CardLayout from '../../layouts/CardLayout';
+import FileComponent from '../../components/FileComponent';
+import { useEffect, useState } from 'react';
+import EventEmitter from '../../services/EventEmitter';
+import ElementTable from '../../components/ElementTable';
+import { RedirectIfAuthRequired } from '../../hooks/SesionHooks';
 const FileUpload = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
-    EventEmitter.addListener("loadData", (data) => {
+    EventEmitter.addListener('loadData', (data) => {
       setData(data);
     });
   }, []);
+  RedirectIfAuthRequired();
   return (
     <div className="col col-lg-7">
       <CardLayout
         icon={Book}
-        header={"Actualización masiva de productos"}
-        title={"Cargar datos desde excel"}
+        header={'Actualización masiva de productos'}
+        title={'Cargar datos desde excel'}
         component={
           <>
-            <FileComponent loadType={"create"} />
+            <FileComponent loadType={'create'} />
             {data && data.length > 0 ? <ElementTable data={data} /> : null}
           </>
         }
